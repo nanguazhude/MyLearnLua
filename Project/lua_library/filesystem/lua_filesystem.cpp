fecf7214aef4f321705c2ee4d287edafbb25da8c
nanguazhude <nanguazhude@vip.qq.com>
2018-04-25 13:02:52 +0800

    +Test Code

---
+++ Project/lua_library/filesystem/lua_filesystem.cpp
@@ -0,0 +1,77 @@
+﻿#ifndef LUA_LIB
+#define LUA_LIB std::size_t(1)
+#endif
+
+#include "lua_filesystem.hpp"
+using namespace std::string_view_literals;
+
+namespace {
+	namespace fs {
+		using namespace filesystem;
+	}/**/
+	[[noreturn]]void __push_lua_error(lua_State*L, const std::string_view &e) {
+		lua_pushlstring(L, e.data(), e.size());
+		lua_error(L);
+	}
+
+	inline void pushlstring(lua_State*L, const std::string_view &e) {
+		lua_pushlstring(L, e.data(), e.size());
+	}
+}/**/
+
+LUA_API LuaFileSystemPath::LuaFileSystemPath(const std::string_view&d) :
+	_m_path(fs::u8path(d.cbegin(), d.cend())) {
+}
+
+namespace {
+	constexpr const char * meta_table_name() {
+		return "sstd::filesystem::path";
+	}
+}
+
+LUA_API int LuaFileSystemPath::register_this(lua_State*L) {
+	luaL_newmetatable(L, meta_table_name());
+	const auto varTableIndex = lua_gettop(L);
+
+	pushlstring(L, u8R"(new)"sv);
+	lua_pushcfunction(L, &LuaFileSystemPath::create_path);
+
+}
+
+namespace {
+	void create_path_make_mtable(lua_State*L, int varInput) {
+		luaL_getmetatable(L, meta_table_name());
+		lua_setmetatable(L, varInput);
+	}
+}/*namespace*/
+
+LUA_API int LuaFileSystemPath::create_path(lua_State*L) {
+	const auto varInput = lua_gettop(L);
+	const auto varType = lua_type(L, varInput);
+	if (varType == LUA_TSTRING) {
+		std::size_t n = 0;
+		const char * d = nullptr;
+		d = lua_tolstring(L, varInput, &n);
+		if (n > 0) {
+			::new(lua_newuserdata(L, sizeof(LuaFileSystemPath)))
+				LuaFileSystemPath(d, n);
+		}
+		else {
+			::new(lua_newuserdata(L, sizeof(LuaFileSystemPath)))
+				LuaFileSystemPath();
+		}
+		create_path_make_mtable(L, lua_gettop(L));
+		return 1;
+	}
+	else if (varType == LUA_TUSERDATA) {
+		auto varP = reinterpret_cast<LuaFileSystemPath *>(
+			luaL_checkudata(L, varInput, meta_table_name()));
+		::new(lua_newuserdata(L, sizeof(LuaFileSystemPath)))
+			LuaFileSystemPath(*varP);
+		create_path_make_mtable(L, lua_gettop(L));
+		return 1;
+	}
+	__push_lua_error(L, u8R"(can not convert input to filesystempath)"sv);
+	return 0;
+}
+/*******************************************/
---
+++ Project/lua_library/filesystem/lua_filesystem.hpp
@@ -0,0 +1,34 @@
+﻿#ifndef __FILE_STSTEM_HPP_LUA
+#define __FILE_STSTEM_HPP_LUA (1L)
+
+#include "../lua.hpp"
+#include <filesystem>
+#include <string_view>
+
+/*using */namespace filesystem = std::experimental::filesystem;
+
+class LuaFileSystemPath {
+	filesystem::path _m_path;
+public:
+	inline LuaFileSystemPath()  = default;
+	inline ~LuaFileSystemPath() = default;
+ 
+	inline operator filesystem::path &() & { return _m_path; }
+	inline operator const filesystem::path &() const & { return _m_path; }
+	inline operator filesystem::path &&() && { return std::move(_m_path); }
+
+	inline LuaFileSystemPath(const LuaFileSystemPath&)=default;
+	inline LuaFileSystemPath(LuaFileSystemPath&&)=default;
+	inline LuaFileSystemPath&operator=(LuaFileSystemPath&)  = default;
+	inline LuaFileSystemPath&operator=(LuaFileSystemPath&&) = default;
+	/*this will alwarys create from utf8*/
+	LUA_API LuaFileSystemPath(const std::string_view&) ;
+	inline LuaFileSystemPath(const char *a,std::size_t b)
+		:LuaFileSystemPath(std::string_view{a,b}) {}
+public:
+	LUA_API static int register_this(lua_State*);
+	LUA_API static int create_path(lua_State*);
+};
+
+#endif
+
